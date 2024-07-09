ab6b83171770585a01ad2d66fc36d8308acf8c51
riz-j <adiputra979@gmail.com>
2024-07-10 00:25:18 +1000

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
 // Define a trait MathOperation
-trait MathOperation<F> {
+trait MathOperation {
     fn operate(&self, a: i32, b: i32) -> i32;
 }
 
 // Implement MathOperation for a generic function F
-impl<F> MathOperation<F> for F
+impl<F> MathOperation for F
 where
     F: Fn(i32, i32) -> i32, // F is a function that takes two i32 and returns an i32
 {
@@ -24,26 +24,42 @@ fn multiply(a: i32, b: i32) -> i32 {
     a * b
 }
 
-struct Calculator<F> {
-    pub methods: HashMap<String, Box<dyn MathOperation<F>>>,
+#[derive(Default)]
+struct Calculator {
+    pub methods: HashMap<String, Box<dyn MathOperation>>,
 }
 
-fn register<F>(function: F) -> i32
-where
-    F: MathOperation<F>,
-{
-    let a = 25;
-    let b = 52;
+impl Calculator {
+    fn register<F>(mut self, method_name: &str, method: F) -> ()
+    where
+        F: MathOperation + 'static,
+    {
+        self.methods
+            .insert(method_name.to_string(), Box::new(method));
+    }
 
-    function.operate(a, b)
+    // fn list_methods(self) -> () {
+    //     println!("{:?}", self.methods);
+    // }
 }
+
+// fn register<F>(function: F) -> i32
+// where
+//     F: MathOperation,
+// {
+//     let a = 25;
+//     let b = 52;
+
+//     function.operate(a, b)
+// }
 
 fn main() {
     // Use the add function with the MathOperation trait
-    // calculator.register("add", add);
-    // calculator.register("multiply", multiply);
-    register(add);
-    register(multiply);
+    let mut calculator = Calculator::default();
+    calculator.register("add", add);
+    // calculator.register("multiply", Box::new(multiply));
+    // register(add);
+    // register(multiply);
 
     let result = add.operate(3, 4);
     println!("Result of addition: {}", result); // Output: "Result of addition: 7"
